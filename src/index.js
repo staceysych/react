22e17b6459a8013f52492cfea08ff1f72dbb57cf
Stacey <stacey.sych@gmail.com>
2020-07-29 18:14:14 +0300

    feat: jsx features

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const todoItems = ['Learn React', 'Build awesome app'];
+
 const TodoList = () => (
   <ul>
-    <li>Learn React</li>
-    <li>Create awesome app</li>
+    <li>{todoItems[0]}</li>
+    <li>{todoItems[1]}</li>
   </ul>
 );
 
 const AppHeader = () => <h1>Mt Todo list</h1>;
+const searchStyle = {
+  fontSize: '25px',
+};
+const searchText = 'Type here to search';
+const SearchPanel = () => (
+  <input
+    placeholder={searchText}
+    style={searchStyle}
+    className="app__input"
+  />
+);
 
-const SearchPanel = () => <input placeholder="Search" />;
+const loginBox = <span>Please, log in</span>;
+const welcomeBox = <span>Welcome Back</span>;
+const isLoggedIn = true;
 
 const App = () => (
   <div>
+    <span>{(new Date()).toString()}</span>
+    <br />
+    {isLoggedIn ? welcomeBox : loginBox}
     <AppHeader />
     <SearchPanel />
     <TodoList />
